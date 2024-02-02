bad730d8d03ba3f6548268d3d662b094a99e61bc
AleksaGavric <aleksagavric1@gmail.com>
2024-02-02 02:00:07 -0500

    upped word count

--- src/components/Interface.tsx
+++ src/components/Interface.tsx
@@ -3,7 +3,7 @@ import { useEffect, useRef, useState } from 'react';
 
 import data from '../../public/text.json';
 
-const wordCount = 4;
+const wordCount = 35;
 
 function getRandomText() {
   const wordList: (string | undefined)[] = [];
