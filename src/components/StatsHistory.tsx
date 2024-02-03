979eba8ee99a4d28e90e50d941a46c220dcf3459
AleksaGavric <aleksagavric1@gmail.com>
2024-02-02 22:04:47 -0500

    added statistics box

--- src/components/StatsHistory.tsx
+++ src/components/StatsHistory.tsx
@@ -67,33 +67,72 @@ const StatsHistory = ({ user }: { user: any }) => {
 
   return (
     <div className="flex h-screen flex-col items-center justify-center p-6 pb-40 md:flex-row">
-      <div className="m-4 flex flex-1 flex-col justify-center bg-white bg-gradient-to-r from-blue-100 to-teal-50 p-4 text-center shadow-xl md:max-h-[45vh] md:max-w-[45vw]">
-        <h1 className="mb-4 text-xl font-bold">Typing History</h1>
-        <div className="max-h-[40vh] overflow-auto">
-          <table className="w-full bg-white">
-            <thead>
-              <tr className="bg-blue-200">
-                <th>Session</th>
-                <th>WPM</th>
-                <th>Time</th>
-              </tr>
-            </thead>
-            <tbody>
-              {history.map((session, index) => (
-                <tr key={index} className="border-b">
-                  <td className="text-center">{history.length - index}</td>
-                  <td className="text-center">{session.wpm}</td>
+      <div className="flex flex-1 flex-col">
+        <div className="mb-4 flex-1 flex-col justify-center bg-white bg-gradient-to-r from-teal-50 to-blue-100 p-4 text-center shadow-xl md:max-h-[35vh] md:max-w-full">
+          <h1 className="mb-4 text-xl font-bold">History</h1>
+          <div className="max-h-[20vh] overflow-auto">
+            <table className="w-full bg-white">
+              <thead>
+                <tr className="bg-blue-200">
+                  <th>Session</th>
+                  <th>WPM</th>
+                  <th>Time</th>
+                </tr>
+              </thead>
+              <tbody>
+                {history.map((session, index) => (
+                  <tr key={index} className="border-b">
+                    <td className="text-center">{history.length - index}</td>
+                    <td className="text-center">{session.wpm}</td>
+                    <td className="text-center">
+                      {new Date(session.time).toLocaleString()}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div className="flex-1 flex-col justify-center bg-white bg-gradient-to-r from-teal-50 to-blue-100 p-4 text-center shadow-lg md:max-h-[35vh] md:max-w-full">
+          <h2 className="mb-4 text-xl font-bold">Statistics</h2>
+          <div className="max-h-[30vh] overflow-auto">
+            <table className="w-full bg-white">
+              <thead>
+                <tr className="bg-blue-200">
+                  <th>Statistic</th>
+                  <th>Value</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="border-b">
+                  <td className="text-center">Best Attempt</td>
                   <td className="text-center">
-                    {new Date(session.time).toLocaleString()}
+                    {Math.max(...history.map((session) => session.wpm))} WPM
                   </td>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+                <tr className="border-b">
+                  <td className="text-center">Average</td>
+                  <td className="text-center">
+                    {Math.ceil(
+                      history.reduce((acc, curr) => acc + curr.wpm, 0) /
+                        history.length
+                    )}{' '}
+                    WPM
+                  </td>
+                </tr>
+                <tr className="border-b">
+                  <td className="text-center">Worst Attempt</td>
+                  <td className="text-center">
+                    {Math.min(...history.map((session) => session.wpm))} WPM
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
       <div className="m-4 flex flex-1 flex-col justify-center bg-white bg-gradient-to-r from-teal-50 to-blue-100 p-4 text-center shadow-lg md:max-h-[50vh] md:max-w-[45vw]">
-        <h2 className="mb-4 text-xl font-bold">Typing Progression</h2>
+        <h2 className="mb-4 text-xl font-bold">Progression</h2>
         {history ? (
           <Line
             data={{
